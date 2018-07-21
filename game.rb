6990c492e7bd6412c281ba648b58bd0b6e6cca3d
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-07-21 15:34:04 +0100

    stuck with the loops

--- game.rb
+++ game.rb
@@ -2,7 +2,7 @@ class Game
 
 	def initialize
 		@points = 501
-	end
+end
 
 	def throw
 		puts "'Throw' to throw your dart"
@@ -13,23 +13,26 @@ class Game
 	def threedart
 		counter = 0
 		sum = 0
-		while counter < 3 do
-			throw
-			score = rand(1..60)
-			puts "You got #{score}"
-			counter += 1
-		end
-		sum += score
-		puts sum
+		array = []
+		totalscore = 501
+		while totalscore <= 501 && totalscore > 80 do
+			while counter < 3 do
+				throw
+				score = rand(1..60)
+				array << score
+				puts "You got #{score}"
+				counter += 1
+				totalscore = totalscore - array.inject(:+)
+				puts totalscore
+			end
+				counter = 0
+			end
 	end
-
-	#def score_total
-
-	#end
 
 
 
 end
 
-game1 = Game.new
-game1.threedart
+game = Game.new
+game1 = game.threedart
+#game2 = game1.threedart
